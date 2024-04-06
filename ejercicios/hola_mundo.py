77952295ba0036382887b918cc684e6b3e2f5fe3
avegat <avegat92@gmail.com>
2024-04-06 11:39:11 -0600

    Agregado módulo para sumar enteros aleatorios y mostrar resultado en hola_mundo-py

---
+++ ejercicios/hola_mundo.py
@@ -0,0 +1,4 @@
+from suma_aleatoria import suma_aleatoria
+
+resultado_suma = suma_aleatoria()
+print(f&quot;Hola mundo: {resultado_suma}&quot;)
---
+++ ejercicios/suma_aleatoria.py
@@ -0,0 +1,7 @@
+import random
+
+def suma_aleatoria():
+    num1 = random.randint(1, 100)
+    num2 = random.randint(1, 100)
+    resultado = num1 + num2    
+    return resultado
